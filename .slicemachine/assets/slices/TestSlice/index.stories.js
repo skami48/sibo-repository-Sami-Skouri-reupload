53bb6808f65453aa7d565970e50840bdb748226b
Sami Skouri <sami.skouri@gmail.com>
2023-03-23 20:11:55 +0100

    remove src (required)

---
+++ .slicemachine/assets/slices/TestSlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TestSlice';
+
+export default {
+  title: 'slices/TestSlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Floor","spans":[]}],"description":[{"type":"paragraph","text":"Veniam velit nisi commodo. Minim cupidatat Lorem voluptate aute ad ut veniam. Officia laborum sint culpa non do laboris Lorem.","spans":[]}]},"id":"_Default","slice_type":"test_slice"}} />
+_Default.storyName = ''
